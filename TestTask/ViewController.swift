65b33cfa56bbf4f880e520aedc51a7e5ccfc7f72
Vasiliy Matveev <thebifid@gmail.com>
2020-06-30 12:37:01 +0700

    Initial Commit

---
+++ TestTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestTask
+//
+//  Created by Vasiliy Matveev on 30.06.2020.
+//  Copyright © 2020 Vasiliy Matveev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
