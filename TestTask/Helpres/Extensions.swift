4aeef07323768a6a4754c1bd30f69de8b8d32a51
Vasiliy Matveev <thebifid@gmail.com>
2020-06-30 16:12:17 +0700

    ext

---
+++ TestTask/Helpres/Extensions.swift
@@ -0,0 +1,18 @@
+//
+//  Extensions.swift
+//  TestTask
+//
+//  Created by Vasiliy Matveev on 30.06.2020.
+//  Copyright © 2020 Vasiliy Matveev. All rights reserved.
+//
+
+import UIKit
+
+extension UIImageView {
+    convenience init(cornerRadius: CGFloat) {
+        self.init(image: nil)
+        self.layer.cornerRadius = cornerRadius
+        self.clipsToBounds = true
+        self.contentMode = .scaleAspectFill
+    }
+}
